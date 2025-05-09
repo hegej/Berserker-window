0ad92b3bfad2179427a098efcfd6318c71073015
hegej <hege.jacobsen12@gmail.com>
2025-05-10 01:33:16 +0200

    Added Berserker mode

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"time"
+	"math/rand"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
@@ -9,10 +9,44 @@ import (
 func main() {
 	screenWidth := int32(800)
 	screenHight := int32(1000)
+	var shakeOffsetX, shakeOffsetY float32
+	var message string
+	var textColor rl.Color
 
 	rl.InitWindow(screenWidth, screenHight, "Berserker Windows")
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
-	rand.seed(time.Now().UnixNano())
+
+	berserker := false
+
+	for !rl.WindowShouldClose() {
+		berserker = rl.IsMouseButtonDown(rl.MouseLeftButton)
+
+		rl.BeginDrawing()
+
+		if berserker {
+			shakeOffsetX = float32(rand.Intn(11) - 5)
+			shakeOffsetY = float32(rand.Intn(11) - 5)
+			message = "The world is at WAR - BERSERKER MODE ON!!!"
+			textColor = rl.Red
+		} else {
+			shakeOffsetX = 0
+			shakeOffsetY = 0
+			message = "The world is at peace..."
+			textColor = rl.White
+		}
+
+		rl.ClearBackground(rl.Black)
+
+		rl.BeginMode2D(rl.NewCamera2D(rl.NewVector2(0, 0), rl.NewVector2(shakeOffsetX, shakeOffsetY), 0, 1.0))
+		rl.DrawText(message, 100, 250, 30, textColor)
+		rl.EndMode2D()
+
+		if berserker {
+			rl.DrawRectangle(0, 0, screenWidth, screenHight, rl.NewColor(255, 0, 0, 80))
+		}
+
+		rl.EndDrawing()
+	}
 }
